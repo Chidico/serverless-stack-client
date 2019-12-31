211bf2be100d46076b6fe7f8ba42f258557d89ac
Chidi Igbelina <chidis@chidis-mbp.attlocal.net>
2019-12-31 16:56:27 -0600

    Setting up React App

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
-import MyComponent from './MyComponent.js';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import "./App.css";
+import Routes from "./Routes";
+import { LinkContainer } from "react-router-bootstrap";
 
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOn: true
-    };
-  }
-  toggle = () => {
-    this.setState({isOn: !this.state.isOn})
-  }
-  render() {
-    return (
-      <div className="App">
-      <div className="App-header"> 
-        <h2>Welcome to React</h2>
-        <MyComponent name="Chidi" /> 
-      </div>
-        <button>
-          {this.state.isOn ? 'On' : 'off'}
-        </button>
-      </div>
-    );
-  }
+function App(props) {
+  return (
+    <div className="App container">
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Scratch</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <LinkContainer to="/signup">
+              <NavItem>Signup</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <NavItem>Login</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "nunu-notes-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://zu202g8pvk.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
+    APP_CLIENT_ID: "us-east-1_desF75F4I",
+    IDENTITY_POOL_ID: "us-east-1:1b8ac9af-315f-4901-a8a7-02d73060b741"
+  }
+};
